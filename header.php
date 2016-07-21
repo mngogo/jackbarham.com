18e3eae2b4923857827594b4cf1b13201df2ee99
jackbarham <jack@airboxmedia.com>
2016-07-21 21:17:32 +0100

    Page title

--- header.php
+++ header.php
@@ -5,15 +5,13 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-    <title><?php wp_title(''); ?></title>
+    <title><?php wp_title('-', true, 'right'); ?>Jack Barham</title>
 
     <meta name="author" content="Jack Barham - http://www.jackbarham.com">
     <meta name="HandheldFriendly" content="True">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-
-    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
     <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
 
     <script src="//use.typekit.net/nmd7zax.js"></script>
